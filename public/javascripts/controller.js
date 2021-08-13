c690a15acb2aba4faf8f88ed10be5307f27d46f6
Vishal Talwar <vtalwar.w@gmail.com>
2021-08-12 21:50:35 -0700

    Updates to fetch users info

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -2,9 +2,31 @@
 
 const e = React.createElement;
 
+const fetchUsers = () => {
+    return fetch('/users')
+        .then(response => response.json())
+        .then(data => {
+            console.log('/users - data: ', data);
+            return Promise.resolve(data);
+        }).catch(error => {
+            console.error('/users - error: ', error);
+            return Promise.reject(error)
+        });
+}
+
 class App extends React.Component {
     constructor(props) {
         super(props);
+    }
+
+    componentDidMount() {
+        fetchUsers().then(result => {
+            console.log("result: %o", result);
+            this.setState({ error: null, users: result })
+        }).catch(error => {
+            console.error('/users - error: %o', error);
+            this.setState({ error: error.message, users: null })
+        });
     }
 
     render() {
